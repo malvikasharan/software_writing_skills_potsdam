bf04347f4f8e0123795bc1001e497ad4bd53e508
Malvika Sharan <malvikasharan@gmail.com>
2015-09-24 16:37:32 +0200

    Create if_name_equals_main.py

---
+++ Python_sessions/session-3/python_codes/if_name_equals_main.py
@@ -0,0 +1,18 @@
+def function_name(variable_1, variable_2):
+'''this tells what the program does'''
+    if variable_1 >= variable_2:
+        print "Do something"
+    elif variable_1 < variable_2:
+        print "Do something different"
+    else:
+        print "Something's wrong"
+
+if __name__ == '__main__': 
+##this physically separates the functions from executions
+## since everything in the left most position is read while running the program
+## Python now reads everything below __name__ == '__main__'
+    variable_1 = sys.argv[1] #user provides this on the commandline "Python script_name.py variable_1"
+    variable_2 = sys.argv[2] #user provides this on the commandline "Python script_name.py variable_1 variable_2"
+    #call_function
+    function_name(variable_1, variable_2)
+    
